f39bae1dfeddd540a1da6569f721f89b912a0fa6
bora <boraoren@gmail.com>
2020-11-22 15:37:04 +1100

    feat(api/transactions): #15 remove optional for account fetch

--- src/main/java/org/anz/wholesale/api/TransactionApi.java
+++ src/main/java/org/anz/wholesale/api/TransactionApi.java
@@ -5,15 +5,14 @@ import org.anz.wholesale.models.Transaction;
 import org.anz.wholesale.repository.AccountRepository;
 import org.anz.wholesale.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -29,15 +28,18 @@ public class TransactionApi {
     }
 
     @GetMapping("/v1/accounts/{accountNumber}/transactions")
-    ResponseEntity<List<Transaction>> getTransactionsBy(@PathVariable("accountNumber") Long accountNumber) {
+    ResponseEntity<List<Transaction>> getTransactionsBy(@PathVariable("accountNumber")
+                                                                Long accountNumber) {
 
-        Optional<Account> optionalAccount = accountRepository.findById(accountNumber);
-        if (optionalAccount.isPresent()) {
+        try {
+            Account account = accountRepository.findBy(accountNumber);
             List<Transaction> transactions = transactionRepository
-                    .findAllByAccount(optionalAccount.get());
+                    .findAllByAccount(account);
             if (transactions.size() > 0) {
                 return ResponseEntity.ok().body(transactions);
             }
+        } catch (EntityNotFoundException entityNotFoundException) {
+            return ResponseEntity.noContent().build();
         }
 
         return ResponseEntity.noContent().build();
--- src/main/java/org/anz/wholesale/repository/AccountRepository.java
+++ src/main/java/org/anz/wholesale/repository/AccountRepository.java
@@ -3,4 +3,12 @@ package org.anz.wholesale.repository;
 import org.anz.wholesale.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccountRepository extends JpaRepository<Account, Long> {}
+import javax.persistence.EntityNotFoundException;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    default Account findBy(Long id) throws EntityNotFoundException{
+        return findById(id).orElseThrow(EntityNotFoundException::new);
+    }
+
+}
